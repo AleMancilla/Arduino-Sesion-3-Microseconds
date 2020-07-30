a215b2d974b42da21c369ad6d08e9bad9c7c9c19
Alejandro <alejandro.mancilla.umsa@gmail.com>
2020-07-29 21:29:59 -0400

    Manejo de microsecons

---
+++ Millis_y_micros/Millis_y_micros.ino
@@ -0,0 +1,14 @@
+// Autor: Alejandro Mancilla Huanca - (+591)65537461
+// Contacto: alejandro.mancilla.umsa@gmail.com
+//
+unsigned long time; 
+void setup()
+{ Serial.begin(9600); } 
+void loop()
+{ Serial.print("Time: ");
+time = millis();
+Serial.println(time); //Imprime el tiempo en millis
+time = micros();
+Serial.println(time); //Imprime el tiempo en micros
+delay(1000); // Espera un segundo para que los datos no se desborden 
+}
---
+++ delayMicrosecons/delayMicrosecons.ino
@@ -0,0 +1,20 @@
+int outPin = 13;                 // digital pin 8
+
+void setup()
+{
+  pinMode(outPin, OUTPUT);      // sets the digital pin as output
+}
+
+void loop()
+{
+  digitalWrite(outPin, HIGH);   // sets the pin on
+  for(int i=0; i<50; i++)
+  {
+    delayMicroseconds(10000);        // pausa por 0.01segundo
+  }
+  digitalWrite(outPin, LOW);    // sets the pin off
+  for(int i=0; i<50; i++)
+  {
+    delayMicroseconds(10000);        // pausa por 0.01segundo
+  }
+}
